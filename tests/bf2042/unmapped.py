d16a1336451bdeb025d82e1758c30f031bf9c34e
Zefanja Jobse <zefanjaj@gmail.com>
2023-07-31 21:15:53 +0200

    add unmapped items from old repo

---
+++ tests/bf2042/unmapped.py
@@ -0,0 +1,68 @@
+# wp
+BF2042_WEAPONS = { 
+    # TODO: Other
+    "": {
+        "type": "Sidearm",
+        "weaponName": "Tracer Dart Gun",
+        "image": "https://cdn.gametools.network/weapons/bf2042/RUM__weapon_default_Tracer_DartGun-3e5a1a6b.png",
+    },
+    "": {
+        "type": "Launcher",
+        "weaponName": "Panzerschreck",
+        "image": "https://cdn.gametools.network/weapons/bf2042/GRA__weapon_default_Panzerschrek-932d81bb.png",
+    },
+}
+# gad
+BF2042_GADGETS = { 
+    # TODO: Other
+    "": {
+        "type": "Gadgets",
+        "gadgetName": "Ranger",
+        "image": "https://cdn.gametools.network/gadgets/bf2042/CallIn_RobotDog-49b0c69d.png",
+    },
+    "": {
+        "type": "Character",
+        "gadgetName": "EMG-X Scanner",
+        "image": "https://cdn.gametools.network/gadgets/bf2042/ARSignalScanner-9c656704.png",
+    },
+    "": {
+        "type": "Character",
+        "gadgetName": "Loadout Crate",
+        "image": "https://cdn.gametools.network/gadgets/bf2042/SupplyBag-dfd654dc.png",
+    },
+    "": {
+        "type": "Character",
+        "gadgetName": "Smart Explosives",
+        "image": "https://cdn.gametools.network/gadgets/bf2042/SmartExplosive0-a9b5fb29.png",
+    },
+    "": {
+        "type": "Character",
+        "gadgetName": "Fortification System",
+        "image": "https://cdn.gametools.network/gadgets/bf2042/ConstructionKit-ef8de994.png",
+    },
+    "": {
+        "type": "Throwable",
+        "gadgetName": "M67 Grenade",
+        "image": "https://cdn.gametools.network/gadgets/bf2042/ALX__FragGrenade-d3e7b908.png",
+    },
+    "": {
+        "type": "Throwable",
+        "gadgetName": "M18 Smoke Grenade",
+        "image": "https://cdn.gametools.network/gadgets/bf2042/M18_SmokeGrenade-dd036d41.png",
+    },
+    "": {
+        "type": "Rocker Launcher",
+        "gadgetName": "FIM-92 Stinger",
+        "image": "https://cdn.gametools.network/gadgets/bf2042/ALX__FIM92a_stinger-4d601d03.png",
+    },
+    "": {
+        "type": "Throwable",
+        "gadgetName": "Mk II Grenade",
+        "image": "https://cdn.gametools.network/gadgets/bf2042/GRA__FragGrenade_001-2c66dc12.png",
+    },
+    "": {
+        "type": "Throwable",
+        "gadgetName": "Stick Grenade",
+        "image": "https://cdn.gametools.network/gadgets/bf2042/GRA__StickGrenade_001-65531a5f.png",
+    },
+}
